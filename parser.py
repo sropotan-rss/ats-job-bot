8178fbfc28c1213d95758f95b6acf95eabe50625
sropotan-rss <sropotan@gmail.com>
2026-03-09 14:40:47 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,14 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def parse_hh(url):
 
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
 
     r = requests.get(url, headers=headers)
 
     soup = BeautifulSoup(r.text, "html.parser")
 
-    text = soup.get_text(" ")
+    text = soup.get_text()
 
-    return text[:7000]
+    return text[:6000]
