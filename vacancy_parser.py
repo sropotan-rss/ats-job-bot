f8d373363507ebeda93a95e91090e33b029e03b4
sropotan-rss <sropotan@gmail.com>
2026-03-09 16:08:43 +0300

    Update vacancy_parser.py

--- vacancy_parser.py
+++ vacancy_parser.py
@@ -1,1 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
 
+def parse_vacancy(url):
+
+    try:
+
+        r = requests.get(url, timeout=10)
+
+        soup = BeautifulSoup(r.text, "html.parser")
+
+        text = soup.get_text()
+
+        return text[:4000]
+
+    except:
+        return url
