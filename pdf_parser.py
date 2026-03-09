ae29c82b07534256528adc534e40354ca3c3b8cb
sropotan-rss <sropotan@gmail.com>
2026-03-09 16:22:15 +0300

    Update pdf_parser.py

--- pdf_parser.py
+++ pdf_parser.py
@@ -1,15 +1,12 @@
 import pdfplumber
 
-def extract_text(path):
+def extract_text(path: str) -> str:
 
     text = ""
 
     with pdfplumber.open(path) as pdf:
-
         for page in pdf.pages:
-
             page_text = page.extract_text()
-
             if page_text:
                 text += page_text + "\n"
 
